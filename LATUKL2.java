94f44d4d8b091b29907d78ae8cdc8be49d370339
unknown <viollae_cahaya_29rpl@student.smktelkom-mlg.sch.id>
2021-07-30 23:40:30 +0700

    yok

---
+++ LATUKL2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latukl2;
+
+/**
+ *
+ * @author admin
+ */
+public class LATUKL2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int i = 4; //deklarasi
+        int l = 1;
+    
+        //perulangan
+        for(int j = l; j <= i; j++){
+           for(int k = l; k <= j; k++){
+               System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+    
